c1ced69277cc1238a6163deeb10d93b0e1a2baac
Ondřej Kocián <kocis.34@gmail.com>
2019-06-07 21:15:06 +0200

    readme, start tests

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,6 +1,47 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { combine, prepare } from '../src';
+import { prepare, combine } from '../src';
+
+function color(state: any, action: any) {
+  switch (action.type) {
+    case 'SET_VALUE':
+      return {
+        ...state,
+        value: action.payload,
+      };
+
+    case 'DO_SOMETHING':
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+function name(state: any, action: any) {
+  switch (action.type) {
+    case 'SET_VALUE':
+      return {
+        ...state,
+        value: action.payload,
+      };
+
+    case 'DO_SOMETHING':
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+const state = {
+  color: {
+    value: '#cc343d',
+  },
+  name: {
+    value: 'semiDarkRed',
+  },
+};
 
 describe('it', () => {
   it('creates Provider and renders without crashing', () => {
@@ -13,4 +54,18 @@ describe('it', () => {
       div
     );
   });
+
+  it('creates combined reducer', () => {
+    const rootReducer = combine({ color, name });
+    const nextState = rootReducer(state, {
+      type: 'SET_VALUE',
+      payload: 'blue',
+    });
+    expect(nextState).toMatchObject({
+      color: { value: 'blue' },
+      name: { value: 'blue' },
+    });
+  });
+
+  it('');
 });
